4b956fb64c496483a8283e6a6386cf850c0223e5
nipa04 <nipa.farjana04@gmail.com>
2018-02-20 10:58:51 -0500

    commit the xcode files

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+var a:Int = 1
+var b = 3
+var e :Float = 2.8
+var v:Float = 3.8
+str = "2"
+print (str)
+
+print (a+b)
+print (a-b)
+print (a*b)
+
+let c = 2
+print (a+c)
+print (c%a)
+var happy = " hello"
+print(happy)
+ happy="helllo to"
+a=4
+print (a+b)
+for index in 1...5 {
+    var sum = 0
+    sum += index
+    print (sum)
+}
+for index in 1...100 {
+    if ((index % 2) == 1) {
+        print (index)
+    }
+}
+
+
