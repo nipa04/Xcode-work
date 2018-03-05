b5c862934c934e7224b7cbd5440efa7122faadc7
nipa04 <nipa.farjana04@gmail.com>
2018-03-05 00:06:19 -0500

    commiting

--- problem13/problem13.playground/Contents.swift
+++ problem13/problem13.playground/Contents.swift
@@ -1,5 +1,25 @@
 //: Playground - noun: a place where people can play
+func temperaturestate ( _ temp: Int) -> String {
+    if temp < 0 {
+        return "Freezing weather"
+    }
+    else if temp <= 10 {
+        return "Very cold weather"
+    }
+    else if temp <= 20 {
+        return "Cold weather"
+    }
+    else if  temp <= 30 {
+        return "Normal in temp"
+    }
+    else if temp <= 40 {
+        return  "Its hot"
+    }
+    else if temp >= 40 {
+        return "Its very hot"
+    }
+    return ""
+}
 
-import UIKit
-
-var str = "Hello, playground"
+let result = temperaturestate( -5)
+print(result)
